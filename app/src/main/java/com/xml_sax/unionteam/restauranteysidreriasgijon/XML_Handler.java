7ee3d03f32b426109a1f4e2e58b0e47af4b51579
Sondarat <jesusmartinmederos4bc@gmail.com>
2018-01-12 11:49:10 +0100

    handler terminado

---
+++ app/src/main/java/com/xml_sax/unionteam/restauranteysidreriasgijon/XML_Handler.java
@@ -0,0 +1,69 @@
+package com.xml_sax.unionteam.restauranteysidreriasgijon;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.util.ArrayList;
+
+
+public class XML_Handler extends DefaultHandler {
+private RestauranteSidreria restauranteActual;
+    private StringBuilder texto;
+    private ArrayList restaurante;
+
+    @Override
+    public void startDocument() throws SAXException {
+        super.startDocument();
+
+    restaurante= new ArrayList();
+    texto=new StringBuilder();
+
+    }
+
+    @Override
+    public void endDocument() throws SAXException {
+        super.endDocument();
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        super.startElement(uri, localName, qName, attributes);
+        if (localName.equals("directorio")) {
+        restauranteActual = new RestauranteSidreria();
+    }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        super.endElement(uri, localName, qName);
+        String text=texto.toString();
+        if(this.restauranteActual!=null){
+            if(localName.equals("foto")){
+                restauranteActual.setFoto(text);
+            }else if(localName.equals("nombre")){
+                restauranteActual.setNombre(text);
+            }else if(localName.equals("telefono")) {
+                restauranteActual.setTlf(text);
+            }else if(localName.equals("correo-electronico")){
+                restauranteActual.setCorreo(text);
+            }else if(localName.equals("web")){
+                restauranteActual.setWeb(text);
+            }else if(localName.equals("horario")){
+                restauranteActual.setHorario(text);
+            }else if(localName.equals("direccion")){
+                restauranteActual.setDireccion(text);
+            }else if(localName.equals("localizacion")){
+                restauranteActual.setCoods(text);
+            }
+        }
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        super.characters(ch, start, length);
+            if(this.restauranteActual!=null){
+                texto.append(ch,start,length);
+            }
+    }
+}
