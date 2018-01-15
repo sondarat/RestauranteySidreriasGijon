30c8d623227d3ebaa1f9f4700d1d345540587538
Sondarat <jesusmartinmederos4bc@gmail.com>
2018-01-15 08:57:35 +0100

    clase parser cambiar cosas cuando gestora este completa

---
+++ app/src/main/java/com/xml_sax/unionteam/restauranteysidreriasgijon/RestauranteSaxParse.java
@@ -0,0 +1,58 @@
+package com.xml_sax.unionteam.restauranteysidreriasgijon;
+
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+/**
+ * Created by DamLocal on 15/01/2018.
+ */
+
+public class RestauranteSaxParse{
+
+    private URL UrlRestaurantes;
+
+    public RestauranteSaxParse(String urlRestaurantes) {
+
+        try{
+            this.UrlRestaurantes = new URL(urlRestaurantes);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public ArrayList<RestauranteSidreria> parse(){
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        try{
+
+
+        SAXParser parser = factory.newSAXParser();
+        XML_Handler handler = new XML_Handler();
+        parser.parse(this.getInputStream(), handler);
+        //Get restaurantes de la gestora molonga
+            //  return handler.getNoticias();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    private InputStream getInputStream(){
+       try
+       {
+        return UrlRestaurantes.openConnection().getInputStream();
+       } catch (IOException e) {
+           throw new RuntimeException(e);
+       }
+    }
+}
