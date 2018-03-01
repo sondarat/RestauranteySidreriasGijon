723ee6ea5e03219ab5e802506d86dadd735740e2
Sondarat <jesusmartinmederos4bc@gmail.com>
2018-03-01 08:44:29 +0100

    widget

---
+++ app/src/main/java/com/xml_sax/unionteam/restauranteysidreriasgijon/LanzarDesdeWidget.java
@@ -0,0 +1,16 @@
+package com.xml_sax.unionteam.restauranteysidreriasgijon;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by DamLocal on 01/02/2018.
+ */
+
+public class LanzarDesdeWidget extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.para_descripcion);
+    }
+}
---
+++ app/src/main/java/com/xml_sax/unionteam/restauranteysidreriasgijon/Widget.java
@@ -0,0 +1,44 @@
+package com.xml_sax.unionteam.restauranteysidreriasgijon;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class Widget extends AppWidgetProvider {
+    GestoraRestaurantes resta;
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+        Intent intent= new Intent(context, LanzarDesdeWidget.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, appWidgetId,intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(R.id.aqui, pendingIntent);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
