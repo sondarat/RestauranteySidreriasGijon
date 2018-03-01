e77df851a51e22b690299af10cc4abd3113cce55
Sondarat <jesusmartinmederos4bc@gmail.com>
2018-03-01 09:31:48 +0100

    widget 2

--- app/src/main/java/com/xml_sax/unionteam/restauranteysidreriasgijon/Widget.java
+++ app/src/main/java/com/xml_sax/unionteam/restauranteysidreriasgijon/Widget.java
@@ -5,20 +5,38 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.widget.ImageView;
 import android.widget.RemoteViews;
+import android.widget.TextView;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class Widget extends AppWidgetProvider {
-    GestoraRestaurantes resta;
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
+
+
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
         Intent intent= new Intent(context, LanzarDesdeWidget.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, appWidgetId,intent,
                 PendingIntent.FLAG_UPDATE_CURRENT);
+
+       /* int numero=(int)(Math.random()*resta.size());
+        RestauranteSidreria cogido=resta.get(numero);
         views.setOnClickPendingIntent(R.id.aqui, pendingIntent);
+
+       CharSequence nombre= (CharSequence) cogido.get(MapRest.NOMBRE);
+        views.setTextViewText(R.id.aquiVaNombreRestaurante,nombre);
+
+        CharSequence horario= (CharSequence) cogido.get(MapRest.HORARIO);
+        views.setTextViewText(R.id.aquiVaHorarioRestaurante,horario);
+
+        Bitmap foto= (Bitmap) cogido.get(MapRest.FOTO);
+        views.setImageViewBitmap(R.id.aquiVaImagenRestaurante,foto);*/
+
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
@@ -34,6 +52,7 @@ public class Widget extends AppWidgetProvider {
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
+
     }
 
     @Override
